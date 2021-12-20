fafbdcf1660f3d1e55227354355017f9b6324872
Xruptor <202515+Xruptor@users.noreply.github.com>
2021-12-20 10:46:20 -0500

    no message

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ from sqlalchemy import create_engine
 
 
 uri = os.getenv("DATABASE_URL")  # or other relevant config var
-if uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://", 1)
+#if uri.startswith("postgres://"):
+#    uri = uri.replace("postgres://", "postgresql://", 1)
 # rest of connection code using the connection string `uri`
 
 try:
