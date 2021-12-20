26c8c9bfb4b729d11d41ccd4b6ca7a18ad7d6617
Xruptor <202515+Xruptor@users.noreply.github.com>
2021-12-20 09:23:32 -0500

    testing table

--- main.py
+++ main.py
@@ -7,9 +7,26 @@ import psycopg2
 from urllib.parse import urlparse
 
 try:
-    urlparse.uses_netloc.append("postgres") 
-    connection_params = urlparse.urlparse(os.environ["DATABASE_URL"])
-    db_connection = psycopg2.connect(database = connection_params.path[1:], user = connection_params.username, password = connection_params.password, host = connection_params.hostname, port = connection_params.port)
-	print "Database connection passed."
+
+    url = urlparse.urlparse(os.environ["DATABASE_URL"])
+    dbname = url.path[1:]
+    user = url.username
+    password = url.password
+    host = url.hostname
+    port = url.port
+
+    con = psycopg2.connect(
+        dbname=dbname, user=user, password=password, host=host, port=port
+    )
+
+    # Open a cursor to perform database operations
+    cur = con.cursor()
+
+    # Execute a query
+    cur.execute("CREATE TABLE IF NOT EXISTS discordbot (keyname, value)")
+
+    print "Database connection passed."
+
+
 except:
     print "Database connection failed."
