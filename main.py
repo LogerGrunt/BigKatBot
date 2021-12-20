b84625bfb61bade7bd98620758f761c28cd40ba7
Xruptor <202515+Xruptor@users.noreply.github.com>
2021-12-20 10:44:09 -0500

    no message

--- main.py
+++ main.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import os
+import re
 import nextcord
 import logging
 import keep_alive
-import psycopg2
-import urllib.parse as urlparse
+#import urllib.parse as urlparse
 from nextcord.ext import commands
+from sqlalchemy import create_engine
+
+
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+# rest of connection code using the connection string `uri`
 
 try:
 
-    url = urlparse.urlparse(os.environ["DATABASE_URL"])
-    dbname = url.path[1:]
-    user = url.username
-    password = url.password
-    host = url.hostname
-    port = url.port
-
-    con = psycopg2.connect(
-        dbname=dbname, user=user, password=password, host=host, port=port
-    )
+    db = create_engine(uri)
 
     print("Database connection passed.")
 except:
@@ -28,14 +26,6 @@ except:
 
 
 try:
-
-    # Open a cursor to perform database operations
-
-    cur = con.cursor()
-
-    # Execute a query
-
-    # cur.execute("INSERT INTO discordbot (keyname, value) VALUES ('Test1', 'test2')")
 
     cur.execute(
         "CREATE TABLE IF NOT EXISTS users (first_name text, last_name text, company text)"
@@ -51,11 +41,6 @@ except:
 
 
 try:
-
-    # Open a cursor to perform database operations
-
-    cur = con.cursor()
-
 
     # Read
     result_set = cur.execute("SELECT * FROM users")
