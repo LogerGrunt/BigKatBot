82fdce138550ceb4a6e6421b93b52add26e6f119
Xruptor <202515+Xruptor@users.noreply.github.com>
2021-12-20 11:55:28 -0500

    no message

--- dbwrapper.py
+++ dbwrapper.py
@@ -9,34 +9,35 @@ import psycopg2
 import urllib.parse as urlparse
 from nextcord.ext import commands
 
-class DiscordDB:
 
+class DiscordDB:
     def __init__(self):
         self.con = None
         self.cur = None
 
-	def Connect(self):
+    def Connect(self):
 
-		url = urlparse.urlparse(os.environ['DATABASE_URL'])
-		dbname = url.path[1:]
-		user = url.username
-		password = url.password
-		host = url.hostname
-		port = url.port
+        url = urlparse.urlparse(os.environ["DATABASE_URL"])
+        dbname = url.path[1:]
+        user = url.username
+        password = url.password
+        host = url.hostname
+        port = url.port
 
-		self.con = psycopg2.connect(dbname=dbname, user=user,
-				password=password, host=host, port=port)
-		self.con.autocommit = True
+        self.con = psycopg2.connect(
+            dbname=dbname, user=user, password=password, host=host, port=port
+        )
+        self.con.autocommit = True
 
-	def setDB(self, keyname, value):
+    def setDB(self, keyname, value):
 
-		self.cur = self.con.cursor()
+        self.cur = self.con.cursor()
 
-		self.cur.execute("DELETE FROM discordbot WHERE keyname='%s';"
-						 , keyname)
-		self.cur.execute('INSERT INTO discordbot (keyname, value) VALUES (%s, %s);'
-						 , (keyname, value))
+        self.cur.execute("DELETE FROM discordbot WHERE keyname='%s';", keyname)
+        self.cur.execute(
+            "INSERT INTO discordbot (keyname, value) VALUES (%s, %s);", (keyname, value)
+        )
 
-	def Close(self):
+    def Close(self):
 
-		self.con.close()
+        self.con.close()
