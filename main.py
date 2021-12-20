deea9179b99599ad260389a05ab0eee6aa2d7053
Xruptor <202515+Xruptor@users.noreply.github.com>
2021-12-20 11:06:11 -0500

    no message

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import os
-import re
 import nextcord
 import logging
 import keep_alive
-
-# import urllib.parse as urlparse
+import psycopg2
+import urllib.parse as urlparse
 from nextcord.ext import commands
-from sqlalchemy import create_engine
 
-uri = os.getenv("DATABASE_URL")  # or other relevant config var
-if uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://", 1)
-# rest of connection code using the connection string `uri`
+con = None
+cur = None
 
 try:
 
-    db = create_engine(uri, echo=True)
+    url = urlparse.urlparse(os.environ["DATABASE_URL"])
+    dbname = url.path[1:]
+    user = url.username
+    password = url.password
+    host = url.hostname
+    port = url.port
+
+    con = psycopg2.connect(
+        dbname=dbname, user=user, password=password, host=host, port=port
+    )
+    con.autocommit = True
 
     print("Database connection passed.")
 except:
@@ -25,21 +31,37 @@ except:
     print("Database connection failed.")
 
 
-db.execute(
-    "CREATE TABLE IF NOT EXISTS users (first_name text, last_name text, company text)"
-)
-db.commit()
+try:
 
-db.execute(
-    "INSERT INTO users (first_name, last_name, company) VALUES ('Sam', 'Pitcher', 'Looker')"
-)
-db.commit()
+    # Open a cursor to perform database operations
+
+    cur = con.cursor()
+
+    # Execute a query
+
+    # cur.execute("INSERT INTO discordbot (keyname, value) VALUES ('Test1', 'test2')")
+
+    cur.execute(
+        "CREATE TABLE IF NOT EXISTS users (first_name text, last_name text, company text)"
+    )
+    cur.execute(
+        "INSERT INTO users (first_name, last_name, company) VALUES ('Sam', 'Pitcher', 'Looker')"
+    )
+
+    print("SQL Passed.")
+except:
+
+    print("SQL Failed.")
 
 
 try:
 
+    # Open a cursor to perform database operations
+
+    cur = con.cursor()
+
     # Read
-    result_set = db.execute("SELECT * FROM users")
+    result_set = cur.execute("SELECT * FROM users")
     for r in result_set:
         print(r)
 
@@ -49,3 +71,7 @@ try:
 except:
 
     print("SQL-1 Failed.")
+# result_set = cur.execute("SELECT * FROM discordbot")
+
+# for r in result_set:
+#    print(r)
