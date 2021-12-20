d0aed4ceb5b6a48c90803759df31e2fc4b3483d7
Xruptor <202515+Xruptor@users.noreply.github.com>
2021-12-20 10:16:55 -0500

    no message

--- main.py
+++ main.py
@@ -8,30 +8,44 @@ import psycopg2
 import urllib.parse as urlparse
 from nextcord.ext import commands
 
-url = urlparse.urlparse(os.environ["DATABASE_URL"])
-dbname = url.path[1:]
-user = url.username
-password = url.password
-host = url.hostname
-port = url.port
+try:
 
-con = psycopg2.connect(
-    dbname=dbname, user=user, password=password, host=host, port=port
-)
+    url = urlparse.urlparse(os.environ["DATABASE_URL"])
+    dbname = url.path[1:]
+    user = url.username
+    password = url.password
+    host = url.hostname
+    port = url.port
 
-# Open a cursor to perform database operations
+    con = psycopg2.connect(
+        dbname=dbname, user=user, password=password, host=host, port=port
+    )
 
-cur = con.cursor()
+    print("Database connection passed.")
+except:
 
-# Execute a query
-
-cur.execute("INSERT INTO discordbot (keyname, value) VALUES ('Test1', 'test2')")
-
-# Read
-
-result_set = cur.execute("SELECT * FROM discordbot")
-
-for r in result_set:
-    print(r)
+    print("Database connection failed.")
 
 
+try:
+
+    # Open a cursor to perform database operations
+
+    cur = con.cursor()
+
+    # Execute a query
+
+    cur.execute("INSERT INTO discordbot (keyname, value) VALUES ('Test1', 'test2')")
+
+    # Read
+
+    print("SQL Passed.")
+except:
+
+    print("SQL Failed.")
+
+
+# result_set = cur.execute("SELECT * FROM discordbot")
+
+# for r in result_set:
+#    print(r)
