ffdb376c830d9114678df2a5bd5359a2ebe1bbb2
Xruptor <202515+Xruptor@users.noreply.github.com>
2021-12-20 10:09:50 -0500

    no message

--- main.py
+++ main.py
@@ -8,35 +8,30 @@ import keep_alive
 import psycopg2
 from urllib.parse import urlparse
 
-try:
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+dbname = url.path[1:]
+user = url.username
+password = url.password
+host = url.hostname
+port = url.port
 
-    url = urlparse.urlparse(os.environ["DATABASE_URL"])
-    dbname = url.path[1:]
-    user = url.username
-    password = url.password
-    host = url.hostname
-    port = url.port
+con = psycopg2.connect(
+    dbname=dbname, user=user, password=password, host=host, port=port
+)
 
-    con = psycopg2.connect(
-        dbname=dbname, user=user, password=password, host=host, port=port
-    )
+# Open a cursor to perform database operations
 
-    # Open a cursor to perform database operations
+cur = con.cursor()
 
-    cur = con.cursor()
+# Execute a query
 
-    # Execute a query
+cur.execute("INSERT INTO discordbot (keyname, value) VALUES ('Test1', 'test2')")
 
-    cur.execute("INSERT INTO discordbot (keyname, value) VALUES ('Test1', 'test2')")
+# Read
 
-    # Read
+result_set = cur.execute("SELECT * FROM discordbot")
 
-    result_set = cur.execute("SELECT * FROM discordbot")
+for r in result_set:
+    print(r)
 
-    for r in result_set:
-        print(r)
 
-    print("Database connection passed.")
-except:
-
-    print("Database connection failed.")
